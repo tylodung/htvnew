86d20581481b8e4b71fb39123d5545416d539f25
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-03-31 13:52:37 +0700

    up

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     blogPostDir: "src/pages/blog", // The name of directory that contains your posts.
-    siteTitle: "Website nhóm HTV21103102", // Site title.
-    siteTitleAlt: "Website nhóm HTV21103102", // Alternative site title for SEO.
+    siteTitle: "Website Đoàn Bình", // Site title.
+    siteTitleAlt: "Website Đoàn Bình", // Alternative site title for SEO.
     siteLogo: "https://api.adorable.io/avatars/150/logo.png", // Logo used for SEO and manifest.
     siteUrl: "http://phieulang.com/", // Domain of your website without pathPrefix.
     pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
@@ -9,8 +9,8 @@ module.exports = {
     siteRss: "/rss.xml", // Path to the RSS file.
     siteFBAppID: "1825356251115265", // FB Application ID for using app insights
     googleAnalyticsID: "UA-IDHERE-5", // GA tracking ID.
-    userName: "Thảo Am", // Username to display in the author segment.
-    copyright: "Copyright © 2017. Thảo Am", // Copyright string for the footer of the website and RSS feed.
+    userName: "Đoàn Bình", // Username to display in the author segment.
+    copyright: "Copyright © 2017. Đoàn Bình", // Copyright string for the footer of the website and RSS feed.
     themeColor: "#663399", // Used for setting manifest and progress theme colors.
     backgroundColor: "#0086b3" // Used for setting manifest background color.
   };
