855c491a14d82be99a0409897dec3aca1f30b9ba
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-03-31 10:48:19 +0700

    up

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ import youtubeLogo from '../img/youtube-logo.svg'
 const Nav = ( { isHome } ) => 
     <nav className="site-nav">
         <div className="site-nav-left">
-            <a className="site-nav-logo" href="/">Website nhóm HTV21103102</a>
+            <a className="site-nav-logo" href="/">Website Đoàn Bình</a>
             <ul className="nav" role="menu">
                 <li role="menuitem">
                     <Link to="/">
