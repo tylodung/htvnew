fc8b985e5c37b32c403f3d16b2d85a84f8b4edfe
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-06-15 10:20:50 +0700

    up

--- src/templates/index.js
+++ src/templates/index.js
@@ -20,7 +20,7 @@ const IndexPage = ({ data, pathContext }) => {
             <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
         </Helmet>
 
-      <Header image='https://res.cloudinary.com/dz09rnbhe/image/upload/v1511348529/ori-and-the-blind-forest-wallpaper-hd_qffhn3.jpg' title="Câu Chuyện Bí Mật" tagline="Mở Cửa Trái Tim" />
+      <Header image='https://res.cloudinary.com/dz09rnbhe/image/upload/v1511348527/braid-hourglass_jso3su.jpg' title="Câu Chuyện Bí Mật" tagline="Mở Cửa Trái Tim" />
       
         <main id="site-main" className="site-main outer" role="main">
 
